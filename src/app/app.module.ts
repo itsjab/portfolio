c8c1f9fd233e3295de87a2f91a8521497d0ba6d3
jan <jan.mayer@campus.lmu.de>
2017-03-28 22:54:27 +0200

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpModule } from '@angular/http'
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
 import { ProjectListComponent } from './components/project-list/project-list.component';
 import { IntroComponent } from './components/intro/intro.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
-import { ProjectService } from './services/project.service';
+import { SchipsComponent } from './schips/schips.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { FrozenSeaComponent } from './frozen-sea/frozen-sea.component';
+import { BuntesDeutschlandComponent } from './buntes-deutschland/buntes-deutschland.component';
+import { SocialRideComponent } from './social-ride/social-ride.component';
+import { TimComponent } from './tim/tim.component';
+import { RoommateComponent } from './roommate/roommate.component';
 
 @NgModule({
   declarations: [
@@ -15,13 +22,21 @@ import { ProjectService } from './services/project.service';
     ProjectListComponent,
     IntroComponent,
     NavigationComponent,
+    SchipsComponent,
+    ProjectsComponent,
+    FrozenSeaComponent,
+    BuntesDeutschlandComponent,
+    SocialRideComponent,
+    TimComponent,
+    RoommateComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
-  providers: [ProjectService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/project-list/project-list.component.ts
+++ src/app/components/project-list/project-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Project} from "../../project";
 import {Http} from "@angular/http";
 import 'rxjs/add/operator/map';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-project-list',
@@ -15,7 +16,9 @@ export class ProjectListComponent implements OnInit {
   mode = 'observable';
 
 
-  constructor(private http: Http) { }
+  constructor(
+    private http: Http,
+    private router: Router) { }
 
   ngOnInit() {
     this.http.get('../../assets/files/projects.json')
@@ -23,6 +26,10 @@ export class ProjectListComponent implements OnInit {
       .subscribe(data => this.projects = data,
         err => console.log(err),
         () => console.log('Completed'));
+  }
+
+  selectProject(project) {
+    this.router.navigate([`/${project}`])
   }
 }
 
